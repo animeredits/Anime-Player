46dad40529b19f6a97fa459380620a81b784c2fe
animeredits <138198032+animeredits@users.noreply.github.com>
2025-01-28 11:49:00 +0530

    devTools off

--- src/main.js
+++ src/main.js
@@ -70,7 +70,7 @@ function createWindow() {
     win.maximize();
     createTray();
     setThumbarButtons();
-    win.webContents.openDevTools();
+    // win.webContents.openDevTools();
   });
 
   win.on("show", setThumbarButtons);
